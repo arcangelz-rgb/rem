34f7872ea04d95950d64109d08b618351c2d6023
arcangelz-rgb <mikeoku2006@gmail.com>
2024-12-02 20:06:05 +0000

    Add files via upload

---
+++ assets/Documents/project/example/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout << "Enter Your name"<<endl;
+    cin>>name;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+
+    cout<<"your name is "<< name<<endl;
+    cout << " your age is " << age<<endl;
+    return 0;
+}
